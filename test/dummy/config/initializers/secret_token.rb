c000f3f98f4ee1e44dda4b44250a9600a54d3d96
Veera <veera@ixl-group.com>
2013-09-18 12:59:37 +0530

    initial updates - rails 4 view pdf renderer support plugin

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '6a1990c4c11a41da38b1a62940a7a2217545f662fcda674c2d9b9986be2b41d0b6892f5e73c2deafdf532a5c84edf63208be64175c620398be57843b0ced34b5'
