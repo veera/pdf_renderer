40a8f300b66566a01cc91f7d3cb6553844e995c0
Veera <veera@ixl-group.com>
2013-09-18 14:53:56 +0530

    Readme instruction updated

--- test/dummy/app/controllers/home_controller.rb
+++ test/dummy/app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @welcome_text = "This sample content goes as pdf format"
     respond_to do |format|
       format.html
       format.pdf { render pdf: "contents" }
